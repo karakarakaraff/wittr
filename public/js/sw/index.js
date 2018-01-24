3e9154ccd20526e6b051de40f835358175414a00
Kara <kara.a.flaherty@gmail.com>
2018-01-24 11:25:35 -0700

    Added custom response to service worker

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,9 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  event.respondWith(
+    new Response("<p>Hello, world!</p><p class='a-winner-is-me'>Just FYI, I'm a winner!</p>", {
+      headers: {'content-type': 'text/html'}
+    })
+  );
 });
