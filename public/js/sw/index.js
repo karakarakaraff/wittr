4650723a59914a58371847945cc20df062dcd866
Kara <kara.a.flaherty@gmail.com>
2018-01-27 21:45:15 -0700

    Swapped the root page for the skeleton page

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,4 +1,4 @@
-var staticCacheName = 'wittr-static-v3';
+var staticCacheName = 'wittr-static-v4';
 
 self.addEventListener('install', function(event) {
   // TODO: cache /skeleton rather than the root page
@@ -6,7 +6,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        '/',
+        '/skeleton',
         'js/main.js',
         'css/main.css',
         'imgs/icon.png',
@@ -35,6 +35,14 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
   // TODO: respond to requests for the root page with
   // the page skeleton from the cache
+  var requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname === '/') {
+      event.respondWith(caches.match('/skeleton'));
+      return;
+    }
+  }
 
   event.respondWith(
     caches.match(event.request).then(function(response) {
