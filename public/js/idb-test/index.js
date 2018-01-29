423b0f29ebc1b9fb6d78e861a93fe3794edebf8a
Kara <kara.a.flaherty@gmail.com>
2018-01-28 17:26:00 -0700

    Wrote a key:value pair to the keyval database

--- public/js/idb-test/index.js
+++ public/js/idb-test/index.js
@@ -28,4 +28,10 @@ dbPromise.then(function(db) {
   // TODO: in the keyval store, set
   // "favoriteAnimal" to your favourite animal
   // eg "cat" or "dog"
+  var tx = db.transaction('keyval', 'readwrite');
+  var keyValStore = tx.objectStore('keyval');
+  keyValStore.put('kitten', 'favoriteAnimal');
+  return tx.complete;
+}).then(function() {
+  console.log('Added favoriteAnimal:kitten to keyval');
 });
