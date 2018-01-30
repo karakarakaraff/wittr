ca10602ad71175fbf125daa75e7fedc473cbb0ff
Kara <kara.a.flaherty@gmail.com>
2018-01-30 12:59:46 -0700

    Check the image cache for avatars and return them if they're there -- afterward, go to the network and update every avatar in the cache and fetch avatars that weren't in the cache

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -49,6 +49,10 @@ self.addEventListener('fetch', function(event) {
     }
     // TODO: respond to avatar urls by responding with
     // the return value of serveAvatar(event.request)
+    if (requestUrl.pathname.startsWith('/avatars/')) {
+      event.respondWith(serveAvatar(event.request));
+      return;
+    }
   }
 
   event.respondWith(
@@ -71,6 +75,15 @@ function serveAvatar(request) {
   // to update the entry in the cache.
   //
   // Note that this is slightly different to servePhoto!
+  return caches.open(contentImgsCache).then(function(cache) {
+    return cache.match(storageUrl).then(function(response) {
+      var networkFetch = fetch(request).then(function(networkResponse) {
+        cache.put(storageUrl, networkResponse.clone());
+        return networkResponse;
+      });
+      return response || networkFetch;
+    });
+  });
 }
 
 function servePhoto(request) {
